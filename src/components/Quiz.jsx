5ba3de6cb98503253a7b5432a2a190fd23e67055
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-22 20:53:18 -0600

    shuffle question and answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -7,6 +7,12 @@ const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
 
+  const shuffledQuestions = [...QUESTIONS];
+  shuffledQuestions.sort(() => Math.random() - 0.5);
+
+  const shuffledAnswers = [...shuffledQuestions[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
@@ -15,9 +21,9 @@ const Quiz = () => {
   return (
     <div id="quiz">
       <div id="question">
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <h2>{shuffledQuestions[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+          {shuffledAnswers.map((answer, index) => (
             <li key={index} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
