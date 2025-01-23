7296b3dbcafa7a940a900cd6490f1b79d1a7787a
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-22 20:53:30 -0600

    quiz is complete

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,26 +2,34 @@ import React from "react";
 import { useState } from "react";
 
 import QUESTIONS from "../question";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
 
-  const shuffledQuestions = [...QUESTIONS];
-  shuffledQuestions.sort(() => Math.random() - 0.5);
-
-  const shuffledAnswers = [...shuffledQuestions[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   }
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
   return (
     <div id="quiz">
       <div id="question">
-        <h2>{shuffledQuestions[activeQuestionIndex].text}</h2>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer, index) => (
             <li key={index} className="answer">
