df44bea282ba1b5600653bac3e60bf51a04ef089
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-22 18:42:01 -0600

    data using use state

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Quiz = () => {
-  return (
-    <div>Quiz</div>
-  )
-}
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  
+  return <p>Currently Active Questions</p>;
+};
 
-export default Quiz
+export default Quiz;
