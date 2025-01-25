51b3f09a99b90f94f0720303d28abe424c387fd3
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-24 21:12:46 -0600

    ading key props

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,18 +1,38 @@
 import React from "react";
 import QuestionTimer from "./QuestionTimer";
 import Answers from "./Answers";
+import { useCallback } from "react";
+import { useState } from "react";
 
 export const Question = ({
+    key,
   questionText,
   answers,
   onSelectAnswer,
   selectedAnswer,
   answerState,
 }) => {
+  const [answers, setAnswers] = useState([
+    { selectedAnswer: "", isCorrect: null },
+  ]);
+
+  function handleSelectAnswer(answer) {
+    setAnswers({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+
+    setTimeout(() => {
+        setAnswers({
+            selectedAnswer: answer,
+            isCorrect: null,
+          });
+    }, 1000);
+  }
+
   return (
     <>
       <QuestionTimer
-        
         timer={10000}
         onFinishCountdown={() => onSelectAnswer(null)}
       />
