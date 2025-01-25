a9d54f61cfa822f63e9bb375ab00a0b8b8e09859
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-24 22:39:08 -0600

    summary percentage

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -2,22 +2,33 @@ import React from "react";
 import quizCompleteImg from "../assets/quiz-complete.png";
 import QUESTIONS from "../question";
 const Summary = ({ userAnswers }) => {
+  const skippedAnswers = userAnswers.filter((answer) => answer === null);
+  const correctAnswers = userAnswers.filter(
+    (answer, index) => answer === QUESTIONS[index].answers[0]
+  );
 
+  const skippedAnswersShare = Math.round(
+    (skippedAnswers.length / userAnswers.length) * 100
+  );
+  const correctAnswersShare = Math.round(
+    (correctAnswers.length / userAnswers.length) * 100
+  );
+  const wrongAnswersShare = 100 - skippedAnswersShare - correctAnswersShare;
   return (
     <div id="summary">
       <img src={quizCompleteImg} alt="" />
       <h2>Quiz Completed</h2>
       <div id="summary-stats">
         <p>
-          <span className="number">10%</span>
+          <span className="number">{skippedAnswersShare}%</span>
           <span className="text">skipped</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{correctAnswersShare}%</span>
           <span className="text">answered correctly</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{wrongAnswersShare}%</span>
           <span className="text">answered incorrectly</span>
         </p>
       </div>
